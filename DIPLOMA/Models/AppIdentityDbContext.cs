2629b4c1609038fb72306354cd5fa0986e8a0387
vldmrgrbv <vovagribov@mail.ru>
2019-05-21 00:06:45 +0700

    Add project DIPLOMA

---
+++ DIPLOMA/Models/AppIdentityDbContext.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+
+namespace DIPLOMA.Models
+{
+    public class AppIdentityDbContext : IdentityDbContext<AppUser>
+    {
+        public AppIdentityDbContext(DbContextOptions<AppIdentityDbContext> options)
+            : base(options) { }
+    }
+}
