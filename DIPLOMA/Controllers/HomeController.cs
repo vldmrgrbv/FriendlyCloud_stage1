3ea223f6b4de55d8b0ca9b043779807a369f72f0
vldmrgrbv <vovagribov@mail.ru>
2019-05-22 23:42:58 +0700

    com

--- DIPLOMA/Controllers/HomeController.cs
+++ DIPLOMA/Controllers/HomeController.cs
@@ -5,11 +5,13 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using DIPLOMA.Models;
+using Microsoft.AspNetCore.Authorization;
 
 namespace DIPLOMA.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ViewResult Index() =>
             View(new Dictionary<string, object>
                  { ["Placeholder"] = "Placeholder" });
